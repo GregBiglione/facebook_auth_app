92b6903161173c197bdaa271d291f209303f0de7
GregBiglione <gregory.biglione81@gmail.com>
2024-04-16 16:12:29 +0200

    Modified SplashScreen

--- lib/presentation/screen/splash/splash_screen.dart
+++ lib/presentation/screen/splash/splash_screen.dart
@@ -1,5 +1,10 @@
 import 'package:facebook_auth_app/presentation/resource/color_manager.dart';
+import 'package:facebook_auth_app/presentation/resource/string_manager.dart';
+import 'package:facebook_auth_app/presentation/resource/style_manager.dart';
+import 'package:facebook_auth_app/presentation/resource/value_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,14 +16,44 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
     return Scaffold(
-      backgroundColor: ColorManager.black,
-      body: const Center(
-        child: Text(
-          "Splash Screen",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
+      body: Container(
+        width: ValueManager.infinity,
+        height: ValueManager.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              ColorManager.primary,
+              ColorManager.primaryDark,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image -----------------------------------------------------------
+              FaIcon(
+                FontAwesomeIcons.facebook,
+                color: ColorManager.white,
+                size: ValueManager.v80,
+              ),
+              const SizedBox(
+                height: ValueManager.v20,
+              ),
+              // Title -----------------------------------------------------------
+              Text(
+                StringManager.appName,
+                style: getBoldStyle(
+                  fontSize: ValueManager.v20,
+                  color: ColorManager.white,
+                ),
+              ),
+            ],
           ),
         ),
       ),
