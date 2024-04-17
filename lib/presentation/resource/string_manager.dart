ae9227747715e2f676547c93ebf9fdd6894ee6c8
GregBiglione <gregory.biglione81@gmail.com>
2024-04-17 13:02:24 +0200

    Added _getContentWidget() & _loginTitleWidget() in LoginContent

--- lib/presentation/resource/string_manager.dart
+++ lib/presentation/resource/string_manager.dart
@@ -1,4 +1,8 @@
 class StringManager {
   static const String appName = "Facebook Auth App";
   static const String noRouteFound = "No route found";
+
+  // Login screen --------------------------------------------------------------
+  static const String logInTitle = "Log in";
+  static const String facebookButtonTitle = "Log in with Facebook";
 }
