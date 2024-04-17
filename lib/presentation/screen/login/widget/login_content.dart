14711fdf3c9baad09df9d3741fd2cfe83572a0da
GregBiglione <gregory.biglione81@gmail.com>
2024-04-17 17:14:35 +0200

    Added FacebookButtonWidget

--- lib/presentation/screen/login/widget/login_content.dart
+++ lib/presentation/screen/login/widget/login_content.dart
@@ -1,12 +1,9 @@
-import 'package:facebook_auth_app/presentation/resource/color_manager.dart';
-import 'package:facebook_auth_app/presentation/resource/font_manager.dart';
 import 'package:facebook_auth_app/presentation/resource/string_manager.dart';
-import 'package:facebook_auth_app/presentation/resource/style_manager.dart';
 import 'package:facebook_auth_app/presentation/resource/value_manager.dart';
+import 'package:facebook_auth_app/presentation/widget/facebook_button_widget.dart';
 import 'package:facebook_auth_app/presentation/widget/padding_widget.dart';
 import 'package:facebook_auth_app/presentation/widget/title_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../app/constant.dart';
 
@@ -38,72 +35,12 @@ class _LoginContentState extends State<LoginContent> {
         const SizedBox(
           height: ValueManager.v20,
         ),
-        _facebookButtonWidget(),
+        FacebookButtonWidget(
+          onPressed: () {
+            logger.i("Facebook button clicked");
+          },
+        ),
       ],
     ),
-  );
-
-  //----------------------------------------------------------------------------
-  // Facebook button
-  //----------------------------------------------------------------------------
-
-  Widget _facebookButtonWidget() => GestureDetector(
-    child: Padding(
-        padding: const EdgeInsets.only(
-          left: ValueManager.v16,
-          right: ValueManager.v16,
-        ),
-        child: Container(
-          width: ValueManager.infinity,
-          height: ValueManager.v50,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                ColorManager.primary,
-                ColorManager.primaryDark,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(
-                ValueManager.v30,
-              ),
-            ),
-          ),
-          child: Row(
-            children: [
-              const SizedBox(
-                width: ValueManager.v20,
-              ),
-              // Logo ------------------------------------------------------------
-              FaIcon(
-                FontAwesomeIcons.facebook,
-                color: ColorManager.white,
-                size: ValueManager.v30,
-              ),
-              // Title -----------------------------------------------------------
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    right: ValueManager.v50,
-                  ),
-                  child: Text(
-                    StringManager.facebookButtonTitle,
-                    style: getBoldStyle(
-                      fontSize: FontSize.s18,
-                      color: ColorManager.white,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-    ),
-    onTap: () {
-      logger.i("Facebook button clicked");
-    },
   );
 }
