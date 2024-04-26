232068788e2cb01a23a97b8374940a742cc706e1
GregBiglione <gregory.biglione81@gmail.com>
2024-04-26 21:07:27 +0200

    Added collection in AppModule

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -2,3 +2,4 @@ import 'package:logger/logger.dart';
 
 var logger = Logger();
 const String IS_USER_LOGGED = "isUserLogged";
+const String USER = "users";
--- lib/data/repository/auth/auth_repository_implementer.dart
+++ lib/data/repository/auth/auth_repository_implementer.dart
@@ -1,17 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:facebook_auth_app/app/app_preferences.dart';
+import 'package:facebook_auth_app/app/constant.dart';
 import 'package:facebook_auth_app/app/di/injection.dart';
 import 'package:facebook_auth_app/domain/repository/auth/auth_repository.dart';
 import 'package:facebook_auth_app/domain/utils/state_render.dart';
 import 'package:facebook_auth_app/presentation/resource/string_manager.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:injectable/injectable.dart';
 
 final AppPreferences _appPreferences = getIt<AppPreferences>();
 
 class AuthRepositoryImplementer extends AuthRepository {
   final FirebaseAuth _firebaseAuth;
+  final CollectionReference _usersCollection;
 
-  AuthRepositoryImplementer(this._firebaseAuth);
+  AuthRepositoryImplementer(this._firebaseAuth,
+      @Named(USER) this._usersCollection);
 
   @override
   Future<StateRender> facebookLogin() async {
