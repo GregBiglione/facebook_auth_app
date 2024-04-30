3fce6c84266301d95aadd2d571f9a4f8f66a8396
GregBiglione <gregory.biglione81@gmail.com>
2024-04-30 19:11:07 +0200

    Added StreamBuilder in HomeScreen

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,13 +1,17 @@
 import 'package:facebook_auth_app/app/function.dart';
+import 'package:facebook_auth_app/domain/model/user_data.dart';
+import 'package:facebook_auth_app/domain/utils/state_render.dart';
 import 'package:facebook_auth_app/presentation/resource/color_manager.dart';
 import 'package:facebook_auth_app/presentation/resource/font_manager.dart';
 import 'package:facebook_auth_app/presentation/resource/string_manager.dart';
 import 'package:facebook_auth_app/presentation/resource/style_manager.dart';
+import 'package:facebook_auth_app/presentation/resource/value_manager.dart';
 import 'package:facebook_auth_app/presentation/screen/home/home_view_model.dart';
 import 'package:facebook_auth_app/presentation/screen/home/widget/home_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gradient_app_bar/flutter_gradient_app_bar.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -30,12 +34,46 @@ class HomeScreen extends StatelessWidget {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
-        title: Text(
-          StringManager.homeTitle,
-          style: getBoldStyle(
-            fontSize: FontSize.s16,
-            color: ColorManager.white,
-          ),
+        title: StreamBuilder(
+          stream: viewModel.getUser(),
+          builder: (context, snapshot) {
+            final response = snapshot.data;
+
+            if(response is Success) {
+              final user = response as Success<UserData>;
+
+              return Text(
+                StringManager.homeTitle + user.data.name,
+                style: getBoldStyle(
+                  fontSize: FontSize.s16,
+                  color: ColorManager.white,
+                ),
+                overflow: TextOverflow.ellipsis,
+              );
+            }
+            else if(response is Loading) {
+              return SpinKitFadingCircle(
+                color: ColorManager.white,
+                size: ValueManager.v40,
+              );
+            }
+            else if(response is Error) {
+              final data = response as Error;
+
+              return Center(
+                child: Text(
+                  StringManager.error + data.error,
+                  style: getBoldStyle(
+                    fontSize: FontSize.s16,
+                    color: ColorManager.error,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              );
+            }
+
+            return const SizedBox();
+          },
         ),
         actions: [
           IconButton(
