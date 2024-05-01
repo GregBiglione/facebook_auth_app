f074f12ba2840cebd72e7be8b51b624f4881f058
GregBiglione <gregory.biglione81@gmail.com>
2024-05-01 03:05:32 +0200

    Added setPhoto() & getPhoto() in AppPreferences

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -4,3 +4,4 @@ var logger = Logger();
 const String IS_USER_LOGGED = "isUserLogged";
 const String USER = "users";
 const String IMAGE_PATH = "assets/images";
+const String USER_PHOTO = "userPhoto";
